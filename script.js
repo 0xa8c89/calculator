dcd67c94cb04b3df492720c83f7fb0637c2af394
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-11-21 02:18:10 +0100

    Add operate function

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function multiply(num1, num2) {
 function devide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case 'x':
+            multiply(num1, num2);
+            break;
+        case '/':
+            subtract(num1, num2);
+            break;
+    }
+}
